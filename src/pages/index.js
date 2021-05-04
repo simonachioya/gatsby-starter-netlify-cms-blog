30df9db4f6607c1da274c69fb6ac19207ecae1ab
Simon Achioya <tomiwaachioya@gmail.com>
2021-05-04 13:37:52 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Blog Site/ Senior Managers</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site!</p>
     <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
